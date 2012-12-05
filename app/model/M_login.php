9afcb2d398eb8ec96c32f653cd6c4993898685a5
djay.si10 <djay.si10@gmail.com>
2012-12-05 09:22:57 +0000

    tampilan daftar akun

--- app/model/M_login.php
+++ app/model/M_login.php
@@ -7,7 +7,6 @@
 		
 		function check($username,$password){
 			$query="select * from akun where username=md5('".$username."') and password=md5('".$password."')";
-			echo $query;
 			$result=mysql_query($query);
 			$row=mysql_num_rows($result);
 			if($row>0){
--- app/view/daftar.php
+++ app/view/daftar.php
@@ -58,7 +58,9 @@
 					<td><input class="ttl" type="text" name="tahun" placeholder="tahun" /></td>
 				</tr>
 			</table>
-			<div class="save" next_c=".d_form" next_i="#d_perusahaan_f" prev_c=".d_form" prev_i="#d_akun_f" ><p>Simpan</p></div>
+			<div class="nav">
+				<div class="save" next_c=".d_form" next_i="#d_perusahaan_f" prev_c=".d_form" prev_i="#d_akun_f"><p>Simpan</p></div>
+			</div>
 		</form>
 		
 		<form class="d_form" id="d_perusahaan_f" method="post">
@@ -66,49 +68,83 @@
 				<tr>
 					<td>Nama Perusahaan</td>
 					<td>:</td>
-					<td colspan="3"><input type="text" name="n_depan" placeholder="Contoh: Budi" /></td>
-				</tr>
-				<tr>
-					<td>Nama Belakang</td>
-					<td>:</td>
-					<td colspan="3"><input type="text" name="n_belakang" placeholder="Contoh: Setiawan" /></td>
-				</tr>
-				<tr>
-					<td>Username</td>
-					<td>:</td>
-					<td colspan="3"><input type="text" name="username" placeholder="Contoh: budi_set" /></td>
+					<td colspan="3"><input type="text" name="n_per" placeholder="Contoh: PT. Maju Mundur" /></td>
 				</tr>
 				<tr>
 					<td>Email</td>
 					<td>:</td>
-					<td colspan="3"><input type="text" name="email" placeholder="Contoh: a@mail.com" /></td>
+					<td colspan="3"><input type="text" name="e_per" placeholder="Contoh: a@mail.com" /></td>
 				</tr>
 				<tr>
-					<td>Password</td>
+					<td>No Telepon</td>
 					<td>:</td>
-					<td colspan="3"><input type="password" name="password" placeholder="minimal 6 karakter" /></td>
+					<td colspan="3"><input type="text" name="tel_per" placeholder="Contoh: 085xxxxxxxx" /></td>
 				</tr>
 				<tr>
-					<td>Konfirmasi Password</td>
+					<td>Fax</td>
 					<td>:</td>
-					<td colspan="3"><input type="password" name="k_password" placeholder="minimal 6 karakter" /></td>
+					<td colspan="3"><input type="password" name="f_per" placeholder="Contoh: (031)766xxxx" /></td>
 				</tr>
 				<tr>
-					<td>Jenis Kelamin</td>
+					<td>Alamat</td>
 					<td>:</td>
-					<td colspan="3"><input type="text" name="j_kelamin" placeholder="" /></td>
+					<td colspan="3"><input type="text" name="a_per" placeholder="Contoh: Jl. Arif Rahman" /></td>
 				</tr>
 				<tr>
-					<td>Tanggal Lahir</td>
+					<td>Kota</td>
 					<td>:</td>
-					<td><input class="ttl" type="text" name="tanggal" placeholder="tanggal" /></td>
-					<td><input class="ttl" type="text" name="bulan" placeholder="bulan" /></td>
-					<td><input class="ttl" type="text" name="tahun" placeholder="tahun" /></td>
+					<td colspan="3"><input type="text" name="k_per" placeholder="Contoh: surabaya" /></td>
 				</tr>
+				<tr>
+					<td>Kecamatan</td>
+					<td>:</td>
+					<td colspan="3"><input type="text" name="kec_per" placeholder="Contoh: sukolilo" /></td>
+				</tr>
+				<tr>
+					<td>Kelurahan</td>
+					<td>:</td>
+					<td colspan="3"><input type="text" name="kel_per" placeholder="Contoh: gebang putih" /></td>
+				</tr>
+				<!--<tr>
+					<td>Kota/Kec/Kel</td>
+					<td>:</td>
+					<td><input class="ttl" type="text" name="k_per" placeholder="Kota" /></td>
+					<td><input class="ttl" type="text" name="kec_per" placeholder="Kecamatan" /></td>
+					<td><input class="ttl" type="text" name="kel_per" placeholder="Kelurahan" /></td>
+				</tr>-->
+				
 			</table>
-			<div class="save"><p>Simpan</p></div>
+			<div class="nav">
+				<div class="back" next_c=".d_form" next_i="#d_akun_f" prev_c=".d_form" prev_i="#d_perusahaan_f" ><p>Kembali</p></div>
+				<div class="save" next_c=".konf" next_i="#konfirmasi" prev_c=".d_form" prev_i="#d_perusahaan_f" ><p>Simpan</p></div>
+			</div>
+			
 		</form>
-		
+	
+		<div class="konf" id="konfirmasi" >
+			<div id="pertanyaan_akhir">
+				<h4>Dengan ini saya menyatakan:</h4>
+				<p>
+					informasi yang saya masukkan mengenai detail akun dan detail perusahaan
+					benar.
+				</p>
+			</div>
+			<div class="nav">
+			<form>
+				<table>
+				<tr>
+					<td>
+						<input type="button" class="back" next_c=".d_form" next_i="#d_perusahaan_f" prev_c=".d_form" prev_i="#d_akun_f" value="Kembali">
+					</td>
+					<td>
+						<input type="submit" class="saves" next_c=".d_form" next_i="#d_perusahaan_f" prev_c=".d_form" prev_i="#d_akun_f" value="Proses">
+					</td>
+				</tr>	
+				</table>
+			</form>
+			</div>
+		</div>
+	
 	</div>
 	
 </div>
