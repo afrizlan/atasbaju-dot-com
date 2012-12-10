5b42a99b66c113adb517e37b375ebacb6ae413d1
afrizlan@gmail.com <afrizlan@gmail.com@69ae3a50-3578-f927-2d05-d792c175e5e9>
2012-12-10 03:24:32 +0000

    Form pada halaman admin yang berisi manajemen akun administrator. mulai dari add, edit, hapus, dan tampilkan akun.

---
+++ trunk/app/view/admin_header.php
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="iso-8859-1"?>
+<html>
+  <head>
+    <meta name="generator"
+    content="HTML Tidy for Windows (vers 14 February 2006), see www.w3.org" />
+    <title><?php echo $title?></title>
+	<link rel="stylesheet" type="text/css" href="css/home-style.css"></link>
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/animation.js" ></script>
+  </head>
+  <body>
+	<div id="wrapper">
+		<div id="header">
+			<div id="logo">
+				<a color="red" href=""><h3>ATASBAJU.COM</h3></a>
+			</div>
+		</div>
+		
+		<div id="content">
