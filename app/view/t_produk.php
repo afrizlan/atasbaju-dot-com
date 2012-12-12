a62c84c056c430dfbb6cef7a19ead96bae5b5fa9
litasyaa <litasyaa@gmail.com>
2012-12-12 10:01:18 +0000

    modifikasi form untuk pengisian jenis dan ukuran produk

--- app/view/t_produk.php
+++ app/view/t_produk.php
@@ -13,36 +13,56 @@
 					<td>:</td>
 					<td colspan="3"><input type="text" name="n_id" placeholder="Contoh: 0123456789" /></td>
 				</tr>
+                
 				<tr>
 					<td>Jenis Produk</td>
 					<td>:</td>
-					<td colspan="3"><input type="text" name="j_produk"/></td>
+					<td><label class="control-label" for="j_produk"></label>
+                    <select type="text" name="j_produk">
+                    	<option>Pilih salah satu</option>
+					    <option>Kemeja</option>
+ 					    <option>Kaos</option>
+					</select></td>
 				</tr>
-				<tr>
+				
+                <tr>
 					<td>Merk</td>
 					<td>:</td>
 					<td colspan="3"><input type="text" name="merk" placeholder="Contoh: Djay Collection" /></td>
 				</tr>
+                
 				<tr>
 					<td>Jenis Ukuran</td>
 					<td>:</td>
-					<td colspan="3"><input type="text" name="j_ukuran" /></td>
+					<td><label class="control-label" for="j_ukuran"></label>
+                    <select type="text" name="j_ukuran">
+                    	<option>Pilih salah satu</option>
+					    <option>S</option>
+ 					    <option>M</option>
+                        <option>L</option>
+                        <option>XL</option>
+                        <option>XXL</option>
+					</select></td>
 				</tr>
+                
 				<tr>
 					<td>Jumlah</td>
 					<td>:</td>
 					<td colspan="3"><input type="text" name="jumlah" placeholder="Contoh : 99" /></td>
 				</tr>
+                
 				<tr>
 					<td>Warna</td>
 					<td>:</td>
 					<td colspan="3"><input type="text" name="warna" placeholder="Contoh : hitam" /></td>
 				</tr>
+                
                 <tr>
 					<td>Harga</td>
 					<td>:</td>
 					<td colspan="3"><input type="text" name="harga" placeholder="Contoh : 125000" /></td>
 				</tr>
+                
                 <tr>
 					<td>Keterangan</td>
 					<td>:</td>
@@ -59,14 +79,14 @@
 			  </table>
 
 <?php  
-include ("connect.php"); 
+include ("connect.php");   
   
 if(isset($_POST["simpan"]))
 {  
-	$idnum = $_POST["idnum"];
-	$jenis_produk = $_POST["jenis_produk"];
+	$idnum = $_POST["n_id"];
+	$jenis_produk = $_POST["j_produk"];
     $merk = $_POST["merk"];
-    $jenis_ukuran = $_POST["jenis_ukuran"];
+    $jenis_ukuran = $_POST["j_ukuran"];
 	$jumlah = $_POST["jumlah"];
 	$warna = $_POST["warna"];	
 	$harga = $_POST["harga"];
