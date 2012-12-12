f5253eef6fd8bd09d99a1a7361c15d85f393ffc6
litasyaa@gmail.com <litasyaa@gmail.com@69ae3a50-3578-f927-2d05-d792c175e5e9>
2012-12-12 09:10:34 +0000

    memperbaiki untuk submit dan tanggal masuk

--- trunk/app/view/t_produk.php
+++ trunk/app/view/t_produk.php
@@ -59,9 +59,9 @@
 			  </table>
 
 <?php  
-require_once("connect.php"); 
+include ("connect.php"); 
   
-if($_POST["simpan"] == "Simpan")
+if(isset($_POST["simpan"]))
 {  
 	$idnum = $_POST["idnum"];
 	$jenis_produk = $_POST["jenis_produk"];
@@ -71,10 +71,11 @@ if($_POST["simpan"] == "Simpan")
 	$warna = $_POST["warna"];	
 	$harga = $_POST["harga"];
 	$keterangan = $_POST["keterangan"];
-	$datetime=date("d-m-y h:i:s");
+	//$datetime=date("d-m-y h:i:s");
       
-    $query="INSERT INTO produk( idnum, jenis_produk, merk, jenis_ukuran, jumlah, warna, harga, tanggal_masuk ) 
-		VALUES( '$idnum', '$jenis_produk', '$merk', '$jenis_ukuran', '$jumlah', '$warna', '$harga', '$keterangan' )";   
+    $query="INSERT INTO produk( idnum, jenis_produk, merk, jenis_ukuran, jumlah, warna, harga, keterangan,  tanggal_masuk ) 
+		VALUES( '$idnum', '$jenis_produk', '$merk', '$jenis_ukuran', '$jumlah', '$warna', '$harga', '$keterangan', now() )";
+		   
     $result=mysql_query($query);            
     
 	if($result){  
