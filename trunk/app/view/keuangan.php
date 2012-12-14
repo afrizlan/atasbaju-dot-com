04adefdea12ca9957fe40e5517b906f125a66193
djay.si10@gmail.com <djay.si10@gmail.com@69ae3a50-3578-f927-2d05-d792c175e5e9>
2012-12-14 12:24:40 +0000

    issues 18: Input Hasil Penjualan
    membuat struktur HTML berupa:
    1. form detail produk yang dibeli
       terdiri dari 3 dropdown list dan 2 field:
       - Merk baju
       - Warna baju
       - Ukuran baju
       - Jumlah Terjual
       - Tanggal
    2. form detail pembeli
       Terdiri dari 3 field
       - Nama
       - No.HP
       - Email
    
    Membuat desain CSS untuk dengan 4 grid yang mewakili tiap usecase dalam manajemen keuangan.

---
+++ trunk/app/view/keuangan.php
@@ -0,0 +1,84 @@
+<link rel="stylesheet" type="text/css" href="css/keuangan-style.css"></link>
+<div id="top" class="box">
+	
+	<div id="top_left" class="left">
+		<h3>Penjualan</h3>
+		<form>
+			<div class="left_form">
+				<table>
+					<tr>
+						<td>Merk Baju</td>
+						<td>
+							<select name="merk">
+								<option value="merk1">Merk1</option>
+								<option value="merk2">Merk2</option>
+								<option value="merk2">Merk3</option>
+							</select>
+						</td>
+					</tr>
+					<tr>
+						<td>Warna Baju</td>
+						<td>
+							<select name="warna">
+								<option value="merah">Merah</option>
+								<option value="kuning">Kuning</option>
+								<option value="hijau">Hijau</option>
+							</select>
+						</td>
+					</tr>
+					<tr>
+						<td>Ukuran Baju</td>
+						<td>
+							<select name="ukuran">
+								<option value="s">S</option>
+								<option value="m">M</option>
+								<option value="l">L</option>
+							</select>
+						</td>
+					</tr>
+					<tr>
+						<td>Jumlah Terjual</td>
+						<td><input type="text" name="jumlah"></input></td>
+					</tr>
+					<tr>
+						<td>Tanggal</td>
+						<td><input type="text" name="tanggal"></input></td>
+					</tr>
+				</table>
+			</div>
+			<div class="right_form">
+				<table>
+					<tr>
+						<td>Nama</td>
+						<td><input type="text" name="n_pembeli"></input></td>
+					</tr>
+					<tr>
+						<td>No.HP</td>
+						<td><input type="text" name="hp_pembeli"></input></td>
+					</tr>
+					<tr>
+						<td>Email</td>
+						<td><input type="text" name="e_pembeli"></input></td>
+					</tr>
+				</table>
+			</div>
+		</form>
+	</div>
+
+	<div id="top_right" class="right">
+		<h3>Data Pemasukan</h3>
+	</div>
+	
+</div>
+
+<div id="bottom" class="box">
+
+	<div id="bottom_left" class="left">
+		<h3>Pengeluaran</h3>
+	</div>
+
+	<div id="bottom_right" class="right">
+		<h3>Data Pengeluaran</h3>
+	</div>
+	
+</div>
